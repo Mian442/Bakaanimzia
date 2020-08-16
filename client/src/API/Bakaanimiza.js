15ac21866e52df5dfdf394adcff1f5aba0568dde
Mian442 <62568869+Mian442@users.noreply.github.com>
2020-08-16 23:44:07 +0500

    Update Bakaanimiza.js

--- client/src/API/Bakaanimiza.js
+++ client/src/API/Bakaanimiza.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:4000/api/",
+  baseURL: "https://bakanimzia-react.herokuapp.com/api/",
   headers: {
     "x-auth-token": window.localStorage.getItem("token")
       ? window.localStorage.getItem("token")
